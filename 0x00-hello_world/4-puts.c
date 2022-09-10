668ac4d3f6afa9727663f0d6fd58b2ddb311f49e
fredkd <kimanifredrick807@gmail.com>
2022-09-10 12:25:14 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
-/**
-* main - this is the main function
-* description- starting point of the program
-* Return : 0 as a success
-*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
