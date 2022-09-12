4de352a3a07101a01421d886a4c8d2d6efab1607
fredkd <kimanifredrick807@gmail.com>
2022-09-12 07:41:35 +0300

    use printf to print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Main function entr point
- * Return: Always 0 (Success)
+ * main - This is the start
+ * Return: Always 0 ( Success )
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
