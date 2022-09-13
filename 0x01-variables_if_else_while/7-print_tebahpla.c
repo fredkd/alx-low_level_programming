bf6805ac9e75327d8c86aaff7f52614ae1872140
fredkd <kimanifredrick807@gmail.com>
2022-09-13 04:46:03 +0300

    Print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is the start of code
+ * Return: 0 as success
+ */
+int main(void)
+{
+
+	int i;
+	
+	for (i = 'z'; i >= 'a'; i--)
+	{
+	
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return(0);
+}
