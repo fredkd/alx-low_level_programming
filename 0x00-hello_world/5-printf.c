0c039cbe13f29623f0033ee2cfe3081417b963f0
fredkd <kimanifredrick807@gmail.com>
2022-09-13 13:01:37 +0000

    using printf to print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
- /**
-*
-*Description : using the main function
-*this program prints "programming is like  building a multilingual puzzle
-*Return: 0
-*/
+
+/**
+ * main - entry point
+ * Return: 0 Always (success)
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
