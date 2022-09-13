c4ebe4bfbc866c80909991abcf75b237c1300df6
fredkd <kimanifredrick807@gmail.com>
2022-09-13 03:44:40 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This is the start of code
+ * Return: 0 Always success
+ */
+int main(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+
+		putchar(i);
+
+	putchar('\n');
+	return(0);
+}
