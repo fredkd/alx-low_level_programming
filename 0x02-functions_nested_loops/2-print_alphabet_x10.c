5112e459474d8b710a0e40ae73e49354b2a5b264
fredkd <kimanifredrick807@gmail.com>
2022-09-14 15:55:10 +0000

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times
+ *
+ * Return: Always success
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	int co = 0;
+
+	while (co < 10)
+	{
+		for  (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+	co++;
+	_putchar('\n');
+	}
+
+}
+
