95d9b02caed246c46b66fa0c767c3813c02d5dc3
fredkd <kimanifredrick807@gmail.com>
2022-09-15 14:13:12 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,22 +6,15 @@
  *
  * @n: Number to pass to _islower function
  */
-void test_islower(int n)
+int _islower(int c);
 {
-	int r;
+	if (c >= 'a' && c<= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 
-	r = islower(n);
-	_putchar(r + '0');
-	_putchar('\n');
-}
-
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	test_islower('H');
-	return (0);
 }
