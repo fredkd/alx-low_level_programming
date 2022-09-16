49a5fc7394132edb75faffd7e209320cc03cc645
fredkd <kimanifredrick807@gmail.com>
2022-09-16 16:47:34 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,15 +8,12 @@
 
 int _isdigit(int c)
 {
-	for (c = 0; c <= 10; c++)
+	if ((c >= '0') && (c <= '10'))
 	{
-		if ((c >= '0') && (c <= '10'))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
