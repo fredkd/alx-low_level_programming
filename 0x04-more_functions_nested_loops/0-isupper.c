4953fe0dab3bf73e82cb6039f5127a2ec1af6ed0
fredkd <kimanifredrick807@gmail.com>
2022-09-16 16:35:40 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,18 +8,13 @@
  */
 
 int _isupper(int c)
-{	
-	int c;
-
-	int c = '';
-
+{
 	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	else if((c >= 'a') && (c <= 'z'))
+	else 
 	{	
 		return (0);
 	}
-	return _isupper();
 }
