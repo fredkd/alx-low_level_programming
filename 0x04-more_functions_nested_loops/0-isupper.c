d422fc94da60a12209d4c57ca25ad1c97ec651d8
fredkd <kimanifredrick807@gmail.com>
2022-09-16 16:17:29 +0000

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -20,18 +20,3 @@ int _isupper(int c)
 		return (0);
 	}
 }
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
-}
