6af62a2443f9ae108982948f86b36a89704e5515
fredkd <kimanifredrick807@gmail.com>
2022-09-19 11:08:58 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @*str: parameter used
+ * return: void
+ */
+
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0 ; i != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
