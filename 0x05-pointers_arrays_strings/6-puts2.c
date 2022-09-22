9ffa8b12750186b0ec340ce8de05777836d5adeb
fredkd <kimanifredrick807@gmail.com>
2022-09-22 13:46:20 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints string
+ * @len: parameter used
+ * @i: parameter used
+ * Return : 0 
+ */
+void puts2(char *str);
+{
+	int len, i;
+	len = srtlen(str)
+	for  ( i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
