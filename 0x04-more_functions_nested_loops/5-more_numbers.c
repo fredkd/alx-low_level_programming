141f7a68dfde95511808573be4b57948702437c0
fredkd <kimanifredrick807@gmail.com>
2022-09-22 16:38:40 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,14 @@
 void more_numbers(void)
 {
 	int i, j;
-	i = 0;
-	for (i = 0; i <=10; i++)
+	
+	for (i = 0; i < = 10; i++)
 	{
-		
 		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j);
 		}
 		j++	
 		_putchar('\n');
-		}
 	}
 }
