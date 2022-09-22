bf95351076d16df4403e3e9594bf74063defca8e
fredkd <kimanifredrick807@gmail.com>
2022-09-22 14:57:40 +0000

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,21 +2,15 @@
 
 /**
  * print_numbers - check the code
- *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int i;
-	int j = 0;
 
-	while (j < 9)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 9; i++)
-		{
-			_putchar(i);
-		}
-	j++;
-	_putchar('\n');
+		_putchar(i + '0');
 	}
+	_putchar('\n');
 }
