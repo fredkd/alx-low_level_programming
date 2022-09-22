4c705f6bd8fd239ca459e00265e72c259372278f
fredkd <kimanifredrick807@gmail.com>
2022-09-22 14:24:36 -0700

    string concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,18 @@
 
 char *_strcat(char *dest, char *src);
 {
-	int index = 0, dest_len = 0;
+	int len = 0, i;
 
-	while (dest[index++])
+	while (dest[len])
 	{
-	dest_len++;
+		len++;
 	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		ken += 1;
+	}
+	dest[len] = '\0'
 
-	for (index = 0; src[index]; index++)
-	{
-		dest[dest_len++] - src[index];
-	}
 	return (dest);
 }
