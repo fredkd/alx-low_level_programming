89fbbc5cd6675a1b9891d7cba0d66a8a3b30250c
fredkd <kimanifredrick807@gmail.com>
2022-09-21 18:39:43 -0700

    quote; git commit -m quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+int main(void)
+{
+	if (write(1, ""and that piece of art is useful" - Dora Korpar, 2015-10-19\n" 60) !=60) 
+	{
+	    write(2 , "null\n", 6);
+	    return (-1);
+	}
+	return (0);
+
+
