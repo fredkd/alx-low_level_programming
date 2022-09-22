b3318a11e2fac0e4f2b8d6c0ac55098b2e8a57ad
fredkd <kimanifredrick807@gmail.com>
2022-09-21 18:55:29 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	if (write(1, ""and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59) != 59) 
+	if (write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58) != 58) 
 	{
 	    write(2, "null\n", 6);
 	    return (-1);
