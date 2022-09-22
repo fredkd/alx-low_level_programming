76e5012b592020d3960e6b535bf4b4b4ea109e2e
fredkd <kimanifredrick807@gmail.com>
2022-09-22 13:22:10 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,33 +2,17 @@
 
 /**
  * print_numbers - check the code
-<<<<<<< HEAD
- *
-=======
->>>>>>> 729d91988abf7696a1fcf3c3c635127964f05ef2
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int i;
-<<<<<<< HEAD
-	int j = 0;
 
-	while (j < 9)
-	{
-		for (i = 0; i <= 9; i++)
-		{
-			_putchar(i);
-		}
-	j++;
-	_putchar('\n');
-	}
-=======
-
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <=9; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
->>>>>>> 729d91988abf7696a1fcf3c3c635127964f05ef2
+
+	return (0);
 }
