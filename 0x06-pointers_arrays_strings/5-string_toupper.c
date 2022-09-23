3a1728a1b5e4a7fba1f4feb98e0624dad1a68e13
fredkd <kimanifredrick807@gmail.com>
2022-09-22 17:24:55 -0700

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,17 +6,17 @@
  * Return: string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *x)
 {
-	int i = 0;
-	char x;
-	A = 65, a = 97;
-	while (x[i])
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (x[i] >= 97 && x[i] <= 122)
+		if (n[1] >= 'a' && n[i] <= 'z')
 		{
-			x[i] = x[i] - 32;
+			n[i] = n[i] - 32;
 		}
-		i++;
 	}
+	return (x);
+
 }
