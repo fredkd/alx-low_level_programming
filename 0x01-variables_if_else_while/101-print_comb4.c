8ceb15548f0406cc5f9b0eb24803781da474fb35
fredkd <kimanifredrick807@gmail.com>
2022-09-23 08:02:31 -0700

    hot fix

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the start of the code
+ * Return: 0 always true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 100; i <= 999; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(',');
+
+	return (0);
+}
