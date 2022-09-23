9fb6dc361eecfcc96abb45f442ae9542cda612a6
fredkd <kimanifredrick807@gmail.com>
2022-09-23 08:50:51 -0700

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_most_numbers - print numbers
- * @i: integer used
  * Return: integer
  */
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
-		while (i != 2 && i !=4)
+		while (i != 2 && i != 4)
 		{
 			_putchar(i);
 			i++;
