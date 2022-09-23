0bdd3c5f17141dcf01decdaa7634addc74997f16
fredkd <kimanifredrick807@gmail.com>
2022-09-23 14:48:11 -0700

    hot fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,16 +2,16 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @int *a: first swap integer
- * @int *b: second swap integer
+ * @int a: first swap integer
+ * @int b: second swap integer
  * return : void
  */
 
 void swap_int(int *a, int *b)
 {
-	
+
 	int r;
-	
+
 	r = *a;
 	*a = *b;
 	*b = r;
