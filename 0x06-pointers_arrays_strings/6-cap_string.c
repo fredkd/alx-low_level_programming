3f101ad814f0d94edbecedad48ca2ff71185e33e
fredkd <kimanifredrick807@gmail.com>
2022-09-22 17:12:31 -0700

    hot fix

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/** cap_string - capitalization
+ * @x: first parpam
+ * Return : uppercase
+ */
+char *cap_string(char *x)
+{
+	char spc[] = {32, 9, '\n', ',', '.', '!', '?', '"', '(', ';', ')', '}', '}'};
+	int len = 13;
+	int a = 0, i;
+
+	while (x[a])
+	{
+		i = 0;
+
+		while (i < len)
+		{
+			if ((a == 0 || s[a - 1] == spc[i] && (s[a] >= 97 && s[a] <= 122)))
+			{		
+				s[a] = s[a] - 32;
+			}
+			i++;
+		}
+		a++
+	}
+	return (x);
+}
