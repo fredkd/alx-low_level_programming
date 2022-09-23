57b52e9ddb4b4075cc1dda427d955dd9787bd1bf
fredkd <kimanifredrick807@gmail.com>
2022-09-23 14:54:23 -0700

    hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,15 +3,15 @@
 
 /**
  * puts2 - prints string
- * @len: parameter used
- * @i: parameter used
- * Return : 0 
+ * @str: param used
+ * Return : 0
  */
 void puts2(char *str)
 {
 	int len, i;
+
 	len = strlen(str);
-	for (i = 0; i < len; i+=2)
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
