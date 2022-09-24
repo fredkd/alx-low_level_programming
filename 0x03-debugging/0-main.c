f0a77e8f777db81143c0c2bf9561665becf6a562
fredkd <kimanifredrick807@gmail.com>
2022-09-24 00:43:31 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * @i: param used
  * main - tests function that print if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
