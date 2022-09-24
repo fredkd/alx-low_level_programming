3bbee74a69b88a76f63b7782578f8c2662dca4a2
fredkd <kimanifredrick807@gmail.com>
2022-09-24 00:59:03 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints number
+ * @n: param used
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else if (n = 0)
+	{
+		_putchar(n);
+	}
+	else
+		_putchar(n);
+	_putchar('\n')
+}
