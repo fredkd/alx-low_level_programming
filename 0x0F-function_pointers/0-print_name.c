5444d44d23029522ed21117f45f57ebb6f4f075b
fredkd <kimanifredrick807@gmail.com>
2022-10-13 10:59:33 -0700

    new commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @*name: parameter used
+ *
+ * @*f: function pointer
+ *
+ *Return: void
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != '\0' && f != '\0')
+	{
+		f(name);
+	}
+}
