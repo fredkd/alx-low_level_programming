f0c77a338e20ac74e0b90339ceb5f4ff4f8c9dec
fredkd <kimanifredrick807@gmail.com>
2022-10-15 04:36:39 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ *
+ * *s: param used
+ *
+ * Return: 0 Always (success)
+ */
+
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (s = 'a', s <= 'z'; s++)
+		{
+			if (s != 'a' || s != 'z')
+			{
+				continue;
+			}
+		}
+		
+		if ((s != " \" ") || (s != " - ") || (s != " + "))
+		{
+			continue;
+		}
+
+		for (s = 0; s <= 9; s++)
+		{
+			_putchar(s);
+		}
+	}
+
+	return (0);
+}
