b44350dc6a81b0b4302fd6cec205b8de80979207
unknown <kimanifredrick807@gmail.com>
2022-11-08 10:13:30 +0300

    dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog description
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 
 struct dog
 {
-	char name;
-	float age;
-	char owner;
-}
+	char *name;
+	float *age;
+	char *owner;
+};
 
 #endif
